41213479d1fc7c11a47d720df5d249f38e335380
Mark <mark.condello@sparkgreen.com.au>
2022-08-04 21:58:20 +1000

    included Google maps

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 // 3rd party packages from NPM
-import $ from 'jquery';
+import $ from 'jquery/dist/jquery';
 // import slick from 'slick-carousel';
 
 import "../scss/style.scss"
 
 // Our modules / classes
-// import MobileMenu from './modules/MobileMenu';
+import MobileMenu from './modules/MobileMenu';
 // import HeroSlider from './modules/HeroSlider';
-// import GoogleMap from './modules/GoogleMap';
-
-// import Search from './modules/Search';
+import GoogleMap from './modules/GoogleMap';
+import Search from './modules/Search';
 
 // // Instantiate a new object using our modules/classes
-// var mobileMenu = new MobileMenu();
+var mobileMenu = new MobileMenu();
 // var heroSlider = new HeroSlider();
-// //new instance of GoogleMap class.
-// var googleMap = new GoogleMap();
-
-// var siteSearch =  new Search();
-
-console.log('Index loaded...')
+var googleMap = new GoogleMap();
+var siteSearch =  new Search();
